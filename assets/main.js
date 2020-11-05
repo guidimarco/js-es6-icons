ee2678d8fac4ec1805b5e37594fb8ee726d5519b
Marco Guidi <mark.88g@gmail.com>
2020-11-05 15:57:37 +0100

    HTML CSS JS file vuoti

---
+++ assets/main.js
@@ -0,0 +1,111 @@
+// <1-fold SSSSS ALL FUNCTION SSSSS
+
+// #2-fold SSSSS region SSSSS
+
+// #/2-fold EEEEE endregion EEEEE
+
+// </1-fold> EEEEE ALL FUNCTION EEEEE
+
+$("document").ready(function() {
+    const icons = [
+        {
+            name: 'cat',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'crow',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dog',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dove',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'dragon',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'horse',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'hippo',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'fish',
+            prefix: 'fa-',
+            type: 'animal',
+            family: 'fas'
+        },
+        {
+            name: 'carrot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'apple-alt',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'lemon',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'pepper-hot',
+            prefix: 'fa-',
+            type: 'vegetable',
+            family: 'fas'
+        },
+        {
+            name: 'user-astronaut',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-graduate',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-ninja',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        },
+        {
+            name: 'user-secret',
+            prefix: 'fa-',
+            type: 'user',
+            family: 'fas'
+        }
+    ];
+
+    console.log(icons);
+
+});
